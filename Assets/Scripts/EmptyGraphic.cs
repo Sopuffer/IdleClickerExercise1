428f3733a03f8d59a23af9eae2cf3b41db9c537e
Sopuffer <sophie.ahlberg@forsbergsskola.student.se>
2020-11-02 19:16:41 +0100

    Added first script. Added first UI elements

---
+++ Assets/Scripts/EmptyGraphic.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class EmptyGraphic : MonoBehaviour
+{
+    public int gold;
+    public TMP_Text goldtext;
+    public int GoldAmountPerClick = 5;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        goldtext.text = "Gold: " + gold;
+
+        if(Input.GetMouseButtonDown(0) && isMouseOverUI())
+        {
+            OnClickAddGold();
+
+        }
+    }
+
+    public bool isMouseOverUI()
+    {
+        return EventSystem.current.IsPointerOverGameObject();
+    }
+
+    public void OnClickAddGold()
+    {
+        gold += GoldAmountPerClick;
+    }
+}
